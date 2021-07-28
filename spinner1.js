e74c4f2075e336f643c05852d93c4f7c0293b0d3
Burhan Syed <burhan.syed@outlook.com>
2021-07-28 00:40:20 +0000

    5 lines of code, LETS GOOO

--- spinner1.js
+++ spinner1.js
@@ -1,32 +1,5 @@
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 700);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 900);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 1100);
-
-setTimeout(() => {
-  process.stdout.write('\r\\  ');
-}, 1300);
-
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 1500);
+const lineArr = ['|', '/', '-', '\\', '/', '-', '\\', '|'];
+const delay = index => index === 0 ? 100 : index * 200
+lineArr.forEach((char, index) => {
+  setTimeout(() => process.stdout.write(`\r${char}   `), delay(index));
+});
